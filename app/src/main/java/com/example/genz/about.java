3d0f78b5d7c7dffff0ed3c7ddb9c0b24ee68cafd
Chirag-S-Kotian <ckotian117@gmail.com>
2025-06-23 21:50:30 +0530

    Update import statement in about.java to use the correct package name

--- app/src/main/java/com/example/genz/about.java
+++ app/src/main/java/com/example/genz/about.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import com.example.genz.R;
+import com.example.finora.R;
 
 public class about extends AppCompatActivity {
 
