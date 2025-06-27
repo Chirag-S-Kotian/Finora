c5d4a8f7dd4243bd1bd947ba14bbdef9a4877f70
Chirag-S-Kotian <ckotian117@gmail.com>
2025-06-27 20:52:12 +0530

    homepage kaboom

--- app/src/main/java/com/example/genz/first_home_page.java
+++ app/src/main/java/com/example/genz/first_home_page.java
@@ -64,7 +64,7 @@ public class first_home_page extends AppCompatActivity implements NavigationView
         setContentView(R.layout.activity_first_home_page);
 
         Toolbar toolbar=findViewById(R.id.my_toolbar);
-        toolbar.setTitle("Finora");
+        toolbar.setTitle("cashKeeper");
         toolbar.setTitleTextColor(Color.BLACK);
 
         setSupportActionBar(toolbar);
