19d5e53ab94295244eb3d7bd99ab42ab7ace7db8
Chirag-S-Kotian <ckotian117@gmail.com>
2025-06-27 20:44:58 +0530

    about page customization

--- app/src/main/java/com/example/genz/about.java
+++ app/src/main/java/com/example/genz/about.java
@@ -11,15 +11,12 @@ import com.example.finora.R;
 
 public class about extends AppCompatActivity {
 
-    TextView vname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        vname=findViewById(R.id.vname);
-        vname.setText("v"+String.valueOf(BuildConfig.VERSION_NAME));
 
         ImageView back_arrow=findViewById(R.id.back);
         back_arrow.setOnClickListener(new View.OnClickListener() {
